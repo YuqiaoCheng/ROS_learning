feeb0f84b1e105d0938340691388beffb0a92b21
YuqiaoCheng <16yc80@queensu.ca>
2023-07-24 19:29:11 -0400

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kavin/demo05_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/kavin/demo05_ws/devel;/opt/ros/noetic'.split(';') if '/home/kavin/demo05_ws/devel;/opt/ros/noetic' != '' else []
